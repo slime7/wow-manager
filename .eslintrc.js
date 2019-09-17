f62d3ed608d158d44ad61e2c365747f2ad39b066
Slime7 <slime7@foxmail.com>
2019-09-17 13:49:33 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        'props': true,
+        'ignorePropertyModificationsFor': [
+          'state',
+          'acc',
+          'e',
+        ],
+      },
+    ],
+    'import/no-extraneous-dependencies': [2, {
+      'ignore':
+        [
+          'electron',
+          'vue-cli-plugin-electron-builder/lib'
+        ]
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
